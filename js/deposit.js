0c818e4c3226d323efb2f1a5c863255c1f7a8f29
Cate Speakman <speakmancate@gmail.com>
2019-07-29 10:43:54 -0400

    revisions to fridays project

--- js/deposit.js
+++ js/deposit.js
@@ -4,32 +4,43 @@
 //              the total interest earned.
 //Author: Cate Speakman
 
-function getValue()
+
+/* This function calculates the future value of a one time deposit assuming compound interest.
+* 
+*@param initialDepositField (number) - the amount of the deposit
+*@param intRteField (number) - the annual interest rate
+*@param depositTermField (number) - the length in time of the deposit
+*...
+*@param futureValueField (number) - the future value of the deposit;
+*@param totalInterestedEarnedField - the total interest earned on the deposit
+*/
+
+function getFutureValue()
 {
     let initialDepositField = document.getElementById("initialDeposit").value;
     initialDepositField = Number(initialDepositField);
 
-    let intRteField =document.getElementById("interestRate").value;
-    intRteField =Number(intRteField);
+    let intRteField = document.getElementById("interestRate").value;
+    intRteField = Number(intRteField);
 
-    let depositTermField=document.getElementById("depositTerm").value;
-    depositTermField=Number(depositTermField);
+    let depositTermField = document.getElementById("depositTerm").value;
+    depositTermField = Number(depositTermField);
 
-    let futureValueField= initialDepositField 
+    let futureValueField = initialDepositField 
                            *
                            Math.pow ((1 + ((intRteField/100)/12)), 12*depositTermField);
-    futureValueField=parseFloat(futureValueField.toFixed(2));
+    futureValueField = parseFloat(futureValueField.toFixed(2));
     document.getElementById("futureValue").value=futureValueField.toFixed(2);
     
     
-    let totalInterestEarnedField= futureValueField - initialDepositField;
-    totalInterestEarnedField=parseFloat(totalInterestEarnedField.toFixed(2));
+    let totalInterestEarnedField = futureValueField - initialDepositField;
+    totalInterestEarnedField = parseFloat(totalInterestEarnedField.toFixed(2));
     document.getElementById("totalInterestEarned").value=totalInterestEarnedField.toFixed(2);
 }
 function init()
 {
-    const calculateBtn =document.getElementById("calculateBtn")
-    calculateBtn.onclick=getValue;
+    const calculateBtn = document.getElementById("calculateBtn")
+    calculateBtn.onclick=getFutureValue;
 }
 
 window.onload = init;
